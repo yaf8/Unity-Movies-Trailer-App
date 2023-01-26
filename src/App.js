53a4d260204cb97364c2b18c62e4071edbbdc3f8
Yafet Abebe <yafetema15@gmail.com>
2023-01-26 17:51:36 +0300

    unfinished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieMain from './components/MovieMain';
+import Login from './components/Login';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <span className={"brand"}>Movie Trailer App</span>
+        <Link to="/components/Login"><button className='loginButton'>Login</button></Link>
+        <Link to="/components/MovieMain"><button className='movieButton'>Movies</button></Link>
+
+        
       </header>
+
+      <body className='body'>
+
+      <div className="containerMain">
+        <Routes>
+          <Route path="/components/Login" element={<Login />} />
+          <Route path="/components/MovieMain" element={<MovieMain />} />
+        </Routes>
+      </div>
+          
+      </body>
+
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4L_OoKlFGvV4UHl5rVHuI4D6Rp_hBQ3c",
+  authDomain: "react-movies-e8c00.firebaseapp.com",
+  projectId: "react-movies-e8c00",
+  storageBucket: "react-movies-e8c00.appspot.com",
+  messagingSenderId: "514999509635",
+  appId: "1:514999509635:web:aabc685a1979ae0146b1b6",
+  measurementId: "G-RXK8WT04FM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
